f3e008d64372676a897e5dc2d79c3b9eb7a7a27f
Matheus Fracalossi <matheusfracalossi@MacBook-Pro-de-Matheus.local>
2025-03-28 18:24:55 -0300

    fix: fix styled-components module declaration

--- src/core/types/styled.d.ts
+++ src/core/types/styled.d.ts
@@ -1,7 +1,7 @@
-import 'styled-components';
+import 'styled-components/native';
 import {defaultTheme} from '../themes';
 
 type Theme = typeof defaultTheme;
-declare module 'styled-components' {
+declare module 'styled-components/native' {
   export interface DefaultTheme extends Theme {}
 }
