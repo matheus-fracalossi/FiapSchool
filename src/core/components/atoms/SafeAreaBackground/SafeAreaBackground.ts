dd776b5d6e4f350007b2345f820d1e4be1f382d5
Matheus Fracalossi <matheusfracalossi@MacBook-Pro-de-Matheus.local>
2025-03-28 20:20:05 -0300

    feat: add typography to theme

--- src/core/components/atoms/SafeAreaBackground/SafeAreaBackground.ts
+++ src/core/components/atoms/SafeAreaBackground/SafeAreaBackground.ts
@@ -3,5 +3,5 @@ import styled from 'styled-components/native';
 
 export const SafeAreaBackground = styled(SafeAreaView)`
   flex: 1;
-  background-color: ${({theme}) => theme.background};
+  background-color: ${({theme}) => theme.colors.background};
 `;
--- src/core/themes/default.ts
+++ src/core/themes/default.ts
@@ -1,10 +1,13 @@
 export const defaultTheme = {
-  background: '#000000',
-  cta: '#29F4D5',
-  blur: '#525252',
-  text: {
-    primary: '#FFFFFF',
+  colors: {
+    background: '#000000',
     cta: '#29F4D5',
-    disabled: '#777777',
+    blur: '#525252',
+    text: {
+      primary: '#FFFFFF',
+      cta: '#29F4D5',
+      disabled: '#777777',
+    },
   },
+  typography: {},
 } as const;
